c72b461b5e06bf3a641ceec6ca80eddb515145de
Lisa Vogt <lisavogtsf@Lisas-Mac-Mini.local>
2016-01-19 19:33:57 -0800

    adds bare bones HTML for game, gets ScrambleApp and GameCtrl angular module and controller working

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
-app.js
+// App-level module
+
+var ScrambleApp = angular.module('ScrambleApp', []);
+
+angular.module('ScrambleApp')
+.controller('GameCtrl', ['$scope', '$http', function ($scope, $http) {
+
+	console.log("Game Controller");
+
+}]);
